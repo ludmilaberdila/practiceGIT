8a255add083ebb5d4c625bf4b2c09d763aaf215e
Ludmila Berdila <ludmila.berdila@gmail.com>
2024-01-04 21:22:16 -0600

    i add

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello usa");
         System.out.println("hi Liuda");
+        System.out.println("salut");
         /* his is Ludmila*/
 
     }
