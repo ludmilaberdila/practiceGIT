71e4f9ba9c2af085df270e695ca71bdcc988fd9d
Ludmila Berdila <147953657+ludmilaberdila@users.noreply.github.com>
2024-01-04 21:20:39 -0600

    Update HelloWorld.java
    i add com.

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello usa");
         System.out.println("hi Liuda");
-        /* his is Ludmila*/
+    
+        /* hi Ludmila Berdila*/
 
     }
 }
