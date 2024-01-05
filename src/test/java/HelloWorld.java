a5d1c2369d70a80069d06e41fef67dc217529a21
Ludmila Berdila <147953657+ludmilaberdila@users.noreply.github.com>
2024-01-04 21:16:31 -0600

    Update HelloWorld.java

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello usa");
-        System.out.println("second change");
+        System.out.println("hi Liuda");
         /* his is Ludmila*/
 
     }
